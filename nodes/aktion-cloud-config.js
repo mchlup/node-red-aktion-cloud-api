581146c44ab9f0adcb3572ab5ee3b888e6df7c67
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-17 09:25:09 +0200

    Update aktion-cloud-config.js

--- nodes/aktion-cloud-config.js
+++ nodes/aktion-cloud-config.js
@@ -1,19 +1,14 @@
 module.exports = function(RED) {
     function AktionCloudConfigNode(config) {
         RED.nodes.createNode(this, config);
-        /*
-        this.email = config.email;
-        this.apiKey = config.apiKey;
-        this.apiUrl = config.apiUrl || "https://api.aktion.cloud";
-        */
-        this.email = this.credentials.email;
-        this.apiKey = this.credentials.apiKey;
-        this.apiUrl = config.apiUrl || "https://api.aktion.cloud";
+        this.email = this.credentials && this.credentials.email;
+        this.apiKey = this.credentials && this.credentials.apiKey;
+        this.apiUrl = config.apiUrl || "https://cloud.aktion.cz/api";
     }
     RED.nodes.registerType("aktion-cloud-config", AktionCloudConfigNode, {
         credentials: {
-            email: {type:"text"},
-            apiKey: {type:"password"}
+            email: { type: "text" },
+            apiKey: { type: "password" }
         }
     });
 };
