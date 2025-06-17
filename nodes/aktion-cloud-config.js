c0d3fe34eb309985a43103855ab88724309c5d3c
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-17 09:15:44 +0200

    Update aktion-cloud-config.js
    Konfigurační node – práce s credentials
    V současné verzi kódu konfiguračního nodu se e-mail a API klíč čtou přímo z config.email a config.apiKey
    GitHub
    . Tyto hodnoty ale Node-RED uchovává jako credentials, takže nejsou dostupné v parametru config při vytváření nodu. Výsledkem bylo, že this.email a this.apiKey v config nodu zůstaly undefined, a ostatní nody tak neměly k dispozici přihlašovací údaje. Řešení: Upravit kód config nodu tak, aby využíval this.credentials. Po vytvoření nodu zavoláme RED.nodes.createNode(this, config);

--- nodes/aktion-cloud-config.js
+++ nodes/aktion-cloud-config.js
@@ -1,8 +1,13 @@
 module.exports = function(RED) {
     function AktionCloudConfigNode(config) {
         RED.nodes.createNode(this, config);
+        /*
         this.email = config.email;
         this.apiKey = config.apiKey;
+        this.apiUrl = config.apiUrl || "https://api.aktion.cloud";
+        */
+        this.email = this.credentials.email;
+        this.apiKey = this.credentials.apiKey;
         this.apiUrl = config.apiUrl || "https://api.aktion.cloud";
     }
     RED.nodes.registerType("aktion-cloud-config", AktionCloudConfigNode, {
